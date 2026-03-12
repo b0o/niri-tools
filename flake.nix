f7e604048c597e2afbefb838c823e1547b2afb6b
Maddison Hellstrom <maddy@na.ai>
2026-03-12 01:12:22 -0700

    chore: initial project scaffolding
    Multi-crate workspace with daemon (niri-tools-daemon), client (niri-tools),
    and shared library (niri-tools-common). Includes flake.nix with devshell,
    CI workflows, and dprint configuration.

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
+
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = rust;
+          cargo = rust;
+        };
+
+        niri-tools = rustPlatform.buildRustPackage {
+          pname = "niri-tools";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      in {
+        packages = {
+          default = niri-tools;
+          inherit niri-tools;
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "niri-tools";
+
+          buildInputs = [
+            rust
+            pkgs.dprint
+          ];
+        };
+      }
+    );
+}
